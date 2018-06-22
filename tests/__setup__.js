1ad2801e59badc5108b7a53aa1aca5c8b22fc3f2
Alexander Komarov <ak@akxcv.com>
2018-06-22 12:15:41 +0300

    Disable Vue production tip in tests

--- tests/__setup__.js
+++ tests/__setup__.js
@@ -1,5 +1,8 @@
 import './polyfills/raf'
+import Vue from 'vue'
 
 global.normalizeHTMLString = function normalizeHTMLString (string) {
   return string.replace(/\n\s*/g, '')
 }
+
+Vue.config.productionTip = false
