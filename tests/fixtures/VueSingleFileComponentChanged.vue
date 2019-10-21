81c974d33939521ca1900b4c9b97cf1da41d3feb
fanyer <fanyeer@didichuxing.com>
2019-10-22 04:15:39 +0800

    ADD: coverage for VueWrapper

---
+++ tests/fixtures/VueSingleFileComponentChanged.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span>{{displayname}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'what2',
+    data(){
+        return {
+            displayname: 'VueSingleFileComponentChanged'
+        }
+    },
+    mounted(){
+        console.log(this.message);
+    },
+    methods: {
+      mayLog(e) {
+          console.log(this.displayname);
+      }
+    }
+  }
+</script>
