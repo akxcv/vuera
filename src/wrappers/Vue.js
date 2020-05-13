08b6249ad0e81e6dbfc4bf92d52de1aa6416d662
Jackong <JackongC@gmail.com>
2020-05-13 14:53:44 +0800

    fix React.Fragment

--- src/wrappers/Vue.js
+++ src/wrappers/Vue.js
@@ -8,7 +8,7 @@ const VUE_COMPONENT_NAME = 'vuera-internal-component-name'
 const wrapReactChildren = (createElement, children) =>
   createElement('vuera-internal-react-wrapper', {
     props: {
-      component: () => <>{children}</>,
+      component: () => <React.Fragment>{children}</React.Fragment>,
     },
   })
 
